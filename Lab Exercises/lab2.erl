9109176a2fecc2adfcab160583d26b4aebef543e
juneezee <engzerjun@gmail.com>
2019-02-13 12:38:16 +0000

    Add Lab 2

---
+++ Lab Exercises/lab2.erl
@@ -0,0 +1,16 @@
+-module(lab2).
+-export([do/1, f/0]).
+
+do({output, E})  -> io:format("~~~p~n", [E]);
+do({input, E})   -> io:format("~p~n", [E]);
+do([])           -> ok;
+do([E | MoreEs]) ->
+    do(E),
+    do(MoreEs);
+do(_)            -> io:format("Unknown argument to do/1~n").
+
+f() ->
+    X = 5,
+    Y = X + 1,
+    Z = Y + 1,
+    Y + X.
